3168ee996606971a3b0a8b605954b1cc0987aa06
TakumiSensei <78392416+TakumiSensei@users.noreply.github.com>
2024-12-03 10:58:39 +0000

    Cog非対応に戻す

--- app/cogs/commands.py
+++ app/cogs/commands.py
@@ -2,10 +2,6 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
-# @tree.command(name='hello', description='Say hello to the world!') 
-# async def test(interaction: discord.Interaction): 
-#   await interaction.response.send_message('Hello, World!')
-
 class HelloCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
--- app/main.py
+++ app/main.py
@@ -2,19 +2,14 @@ import os
 import dotenv
 from server import server_thread
 import discord
-from discord.ext import commands
-import cogs
+from discord import app_commands
 
 dotenv.load_dotenv()
 
-INITIAL_EXTENSIONS = [
-    'cogs.commands'
-]
 TOKEN = os.environ.get("TOKEN")
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='/', intents=intents)
-# bot = discord.Client(intents=intents)
-# tree = app_commands.CommandTree(bot)
+bot = discord.Client(intents=intents)
+tree = app_commands.CommandTree(bot)
 
 # botのログインと同期
 @bot.event
@@ -24,16 +19,14 @@ async def on_ready():
     # アクティビティを設定
     game = discord.Game("helpコマンドでbotの使い方をお伝えします。")
     await bot.change_presence(status=discord.Status.idle, activity=game)
-    
-    for cog in INITIAL_EXTENSIONS:
-        await bot.load_extension(cog)
 
     # スラッシュコマンドを同期
-    await bot.tree.sync()
+    await tree.sync()
 
-# @tree.command(name='hello', description='Say hello to the world!') 
-# async def test(interaction: discord.Interaction): 
-#   await interaction.response.send_message('Hello, World!')
+# スラッシュコマンドを設定
+@tree.command(name='hello', description='Say hello to the world!') 
+async def test(interaction: discord.Interaction): 
+  await interaction.response.send_message('Hello, World!')
 
 # トークンを指定してbot実行
 server_thread()
