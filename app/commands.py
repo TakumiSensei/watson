7ba7f090af22f292d6cd19ffa94708894597de7e
TakumiSensei <78392416+TakumiSensei@users.noreply.github.com>
2024-12-03 08:18:19 +0000

    スラッシュコマンドのテスト3

--- app/commands.py
+++ app/commands.py
@@ -1,6 +1,19 @@
 import discord
-from main import tree
+from discord.ext import commands
+from discord import app_commands
 
 # @tree.command(name='hello', description='Say hello to the world!') 
 # async def test(interaction: discord.Interaction): 
 #   await interaction.response.send_message('Hello, World!')
+
+class HelloCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Cog ready!")
+	
+    @app_commands.command(name="hello", description='Say hello to the world!')
+    async def hello(self,interaction:discord.Interaction):
+        await interaction.response.send_message('Hello, World!')
--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import dotenv
 from server import server_thread
 import discord
 from discord import app_commands
-import commands
+from commands import HelloCog
 
 dotenv.load_dotenv()
 
@@ -24,10 +24,11 @@ async def on_ready():
     # スラッシュコマンドを同期
     await tree.sync()
 
-@tree.command(name='hello', description='Say hello to the world!') 
-async def test(interaction: discord.Interaction): 
-  await interaction.response.send_message('Hello, World!')
+# @tree.command(name='hello', description='Say hello to the world!') 
+# async def test(interaction: discord.Interaction): 
+#   await interaction.response.send_message('Hello, World!')
 
 # トークンを指定してbot実行
 server_thread()
+client.add_cog(HelloCog(client))
 client.run(TOKEN)
