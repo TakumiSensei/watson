04e0b166e5d97ffaa8f7bf97428eb4833851b11d
TakumiSensei <78392416+TakumiSensei@users.noreply.github.com>
2024-12-03 12:41:24 +0000

    やっぱりCog対応

--- app/main.py
+++ app/main.py
@@ -2,14 +2,17 @@ import os
 import dotenv
 from server import server_thread
 import discord
-from discord import app_commands
+from discord.ext import commands
 
 dotenv.load_dotenv()
 
+INITIAL_EXTENSIONS = [
+    '.cogs.commands',
+]
 TOKEN = os.environ.get("TOKEN")
+
 intents = discord.Intents.all()
-bot = discord.Client(intents=intents)
-tree = app_commands.CommandTree(bot)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 # botのログインと同期
 @bot.event
@@ -19,14 +22,12 @@ async def on_ready():
     # アクティビティを設定
     game = discord.Game("helpコマンドでbotの使い方をお伝えします。")
     await bot.change_presence(status=discord.Status.idle, activity=game)
+    
+    for cog in INITIAL_EXTENSIONS:
+        await bot.load_extension(cog)
 
     # スラッシュコマンドを同期
-    await tree.sync()
-
-# スラッシュコマンドを設定
-@tree.command(name='test', description='Say hello to the world!') 
-async def test(interaction: discord.Interaction): 
-    await interaction.response.send_message('Hello, World!')
+    await bot.tree.sync()
 
 # トークンを指定してbot実行
 server_thread()
