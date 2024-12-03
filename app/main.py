514ac31d4acf1ef72e663d6bb3432d3f2ea1aa4f
TakumiSensei <78392416+TakumiSensei@users.noreply.github.com>
2024-12-03 12:44:32 +0000

    エラー対応

--- app/main.py
+++ app/main.py
@@ -3,11 +3,12 @@ import dotenv
 from server import server_thread
 import discord
 from discord.ext import commands
+import cogs
 
 dotenv.load_dotenv()
 
 INITIAL_EXTENSIONS = [
-    '.cogs.commands',
+    'cogs.commands',
 ]
 TOKEN = os.environ.get("TOKEN")
 
